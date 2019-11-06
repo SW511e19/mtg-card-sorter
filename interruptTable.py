61f9fb89db4402bdf1c0d58ef2204bef36b151d9
Huntersz <37700694+Cklein17@users.noreply.github.com>
2019-11-06 11:27:27 +0100

    Update interruptTable.py
    added sorting to task_lst, lowest priority is first in the list now

--- interruptTable.py
+++ interruptTable.py
@@ -1,5 +1,4 @@
 import datetime
-import task
 
 
 def get_time_ms():
@@ -8,10 +7,26 @@ def get_time_ms():
 
 class InterruptTable:
     task_lst = []
-    task = task.make_task(5000, 42)
 
-    tuple = (task, task.deadline, task.priority)
-    task_lst.append(tuple)
+    def __init__(self):
+        pass
 
-    def add_task(self, task):
-        self.task_lst.append(task)
+    def add_task(self, _task):
+        task_tuple = (_task, _task.deadline, _task.priority)
+        self.task_lst.append(task_tuple)
+        self.sort_task_lst()
+
+    # insertion sort modified for task priority and tuples
+    def sort_task_lst(self):
+        for i in range(1, len(self.task_lst)):
+            task_priority = self.task_lst[i][2]
+            j = i - 1
+            while j >= 0 and task_priority < self.task_lst[j][2]:
+                temp = self.task_lst[j + 1]
+                self.task_lst[j + 1] = self.task_lst[j]
+                self.task_lst[j] = temp
+                j = j - 1
+
+
+def create_interrupt_table():
+    return InterruptTable()
